f6bd32d2a09a872fa6fbb337efa842571c5a6819
Guillermo Tinoco <m3mo89@gmail.com>
2019-11-08 20:32:40 -0600

    Initial commit

---
+++ DependencyInjectionSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
